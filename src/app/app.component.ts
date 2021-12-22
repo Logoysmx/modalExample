b6a7431425756dd8143aab78fffff5d14bcc147d
Isaac Fraire Heredia <isaacfraireheredia@gmail.com>
2021-12-22 15:22:55 -0600

    awesome fonts added and some features for modal structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  faCoffee = faCoffee;
   @Input() visible: boolean;
   @Output() close: EventEmitter<boolean> = new EventEmitter();
 
